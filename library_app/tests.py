fb495b125e0360c57046adf979052ad540a6023c
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-19 12:50:47 +0100

    Add tests for book listing and author creation endpoints with authentication using RequestsClient and APIRequestFactory.

--- library_app/tests.py
+++ library_app/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient, RequestsClient
+from rest_framework.test import APIClient, RequestsClient, APIRequestFactory, force_authenticate
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from .views import *
 
 class AuthorTests(TestCase):
     def setUp(self):
@@ -19,6 +19,9 @@ class AuthorTests(TestCase):
         response = self.client.post(reverse('author-list'), {'name': 'Jack London', 'nationality': 'American'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_create_author_error(self):
+        response = self.client.post(reverse('author-list'), {'nationality': 'American'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 class BookListTests(TestCase):
     def setUp(self):
@@ -41,5 +44,22 @@ class BookListTestsWithRequestClient(TestCase):
         self.client = RequestsClient()
         self.user = User.objects.create_user(username='testuser123', password='testuser123')
         self.token = RefreshToken.for_user(self.user)
-        self.client.headers.update(f'Authorization: Bearer {self.token.access_token}')
+        self.client.headers.update({'Authorization': f'Bearer {self.token.access_token}'})
+
+
+    def test_list_book(self):
+        response = self.client.get('http://127.0.0.1:8000/api/books/')
+        self.assertEqual(response.status_code, 200)
+
+
+class AuthorCreateTestFactory(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(username='testuser123', password='testuser123', email='test123@test.com')
+
+    def test_create_author_with_factory(self):
+        request = self.factory.post('/authors/', {'name': 'Onore de Balzak', 'nationality': 'French'}, format='json')
+        force_authenticate(request, user=self.user)
+        response = AuthorListCreate.as_view()(request)
+        self.assertEqual(response.status_code, 201)
 
