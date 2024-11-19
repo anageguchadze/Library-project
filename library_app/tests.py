fc77f8194ae5843bdae80560bae53b9d4ef3563d
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-19 16:14:46 +0100

    Added pytest test for author-book relationship

--- library_app/tests.py
+++ library_app/tests.py
@@ -6,60 +6,81 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from .views import *
 
-class AuthorTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
+#pytest
+import pytest
+from django.contrib.auth.models import User
+from .models import *
 
 
-    def test_list_authors(self):
-        response = self.client.get(reverse('author-list'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db
+def test_author_book_relationship():
+    author = Author.objects.create(name='Mikhail Bulgakov', nationality='Russian')
+    Book.objects.create(title='The Master and Margarita', author=author)
+    Book.objects.create(title='The Heart of a Dog', author=author)
 
-    def test_create_author(self):
-        response = self.client.post(reverse('author-list'), {'name': 'Jack London', 'nationality': 'American'}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    assert Book.objects.filter(author=author).count() == 2
 
-    def test_create_author_error(self):
-        response = self.client.post(reverse('author-list'), {'nationality': 'American'}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    author.delete()
 
-class BookListTests(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testuser123')
-        self.token = RefreshToken.for_user(self.user)
-
-    def test_list_books_unauth(self):
-        response = self.client.get(reverse('book-list'))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_list_books_auth(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
-        response = self.client.get(reverse('book-list'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    assert Book.objects.count() == 0
 
 
-class BookListTestsWithRequestClient(TestCase):
-    def setUp(self):
-        self.client = RequestsClient()
-        self.user = User.objects.create_user(username='testuser123', password='testuser123')
-        self.token = RefreshToken.for_user(self.user)
-        self.client.headers.update({'Authorization': f'Bearer {self.token.access_token}'})
 
 
-    def test_list_book(self):
-        response = self.client.get('http://127.0.0.1:8000/api/books/')
-        self.assertEqual(response.status_code, 200)
+# class AuthorTests(TestCase):
+#     def setUp(self):
+#         self.client = APIClient()
 
 
-class AuthorCreateTestFactory(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.user = User.objects.create_user(username='testuser123', password='testuser123', email='test123@test.com')
+#     def test_list_authors(self):
+#         response = self.client.get(reverse('author-list'))
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_author_with_factory(self):
-        request = self.factory.post('/authors/', {'name': 'Onore de Balzak', 'nationality': 'French'}, format='json')
-        force_authenticate(request, user=self.user)
-        response = AuthorListCreate.as_view()(request)
-        self.assertEqual(response.status_code, 201)
+#     def test_create_author(self):
+#         response = self.client.post(reverse('author-list'), {'name': 'Jack London', 'nationality': 'American'}, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+#     def test_create_author_error(self):
+#         response = self.client.post(reverse('author-list'), {'nationality': 'American'}, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+# class BookListTests(TestCase):
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = User.objects.create_user(username='testuser', password='testuser123')
+#         self.token = RefreshToken.for_user(self.user)
+
+#     def test_list_books_unauth(self):
+#         response = self.client.get(reverse('book-list'))
+#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+#     def test_list_books_auth(self):
+#         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+#         response = self.client.get(reverse('book-list'))
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# class BookListTestsWithRequestClient(TestCase):
+#     def setUp(self):
+#         self.client = RequestsClient()
+#         self.user = User.objects.create_user(username='testuser123', password='testuser123')
+#         self.token = RefreshToken.for_user(self.user)
+#         self.client.headers.update({'Authorization': f'Bearer {self.token.access_token}'})
+
+
+#     def test_list_book(self):
+#         response = self.client.get('http://127.0.0.1:8000/api/books/')
+#         self.assertEqual(response.status_code, 200)
+
+
+# class AuthorCreateTestFactory(TestCase):
+#     def setUp(self):
+#         self.factory = APIRequestFactory()
+#         self.user = User.objects.create_user(username='testuser123', password='testuser123', email='test123@test.com')
+
+#     def test_create_author_with_factory(self):
+#         request = self.factory.post('/authors/', {'name': 'Onore de Balzak', 'nationality': 'French'}, format='json')
+#         force_authenticate(request, user=self.user)
+#         response = AuthorListCreate.as_view()(request)
+#         self.assertEqual(response.status_code, 201)
 
